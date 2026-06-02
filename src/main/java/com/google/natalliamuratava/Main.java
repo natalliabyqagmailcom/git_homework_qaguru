20116dfaa8c2f90d2609a2576acc0a584c7ffd25
natallia.by.qa@gmail.com <natallia.by.qa@gmail.com>
2026-06-03 00:39:20 +0300

    change bye

--- src/main/java/com/google/natalliamuratava/Main.java
+++ src/main/java/com/google/natalliamuratava/Main.java
@@ -8,7 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
 
         System.out.printf("Hello world!");
-        System.out.printf("Bye!");
+        System.out.printf("Goodbye!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
